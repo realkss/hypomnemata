d743083426a475921b0d511400eb6e1e1f8d9983
realkss <silentumestaureum@protonmail.ch>
2026-03-05 01:26:41 -0500

    deploy to github pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "realkss.github.io/hypomnemata",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
